1e6665e0c906b676501cecb506da190a4d5df5af
rifkyaditya <kiditzbastara@gmail.com>
2022-10-18 15:25:08 +0700

    remove import

--- order/src/main/kotlin/com/stafsus/practice/order/core/event/events.kt
+++ order/src/main/kotlin/com/stafsus/practice/order/core/event/events.kt
@@ -1,6 +1,5 @@
 package com.stafsus.practice.order.core.event
 
-import com.stafsus.practice.order.command.OrderStatus
 import com.stafsus.practice.order.core.model.OrderStatus
 
 data class OrderCreatedEvent(
