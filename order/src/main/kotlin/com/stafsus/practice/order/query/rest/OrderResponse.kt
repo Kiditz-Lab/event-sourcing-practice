61e6037b211fab9124290c23dfaa878e93bc0ac1
rifkyaditya <kiditzbastara@gmail.com>
2022-10-18 15:24:36 +0700

    add order

--- order/src/main/kotlin/com/stafsus/practice/order/query/rest/OrderResponse.kt
+++ order/src/main/kotlin/com/stafsus/practice/order/query/rest/OrderResponse.kt
@@ -1,9 +1,6 @@
 package com.stafsus.practice.order.query.rest
 
-import com.stafsus.practice.order.command.OrderStatus
 import com.stafsus.practice.order.core.model.OrderStatus
-import org.hibernate.Hibernate
-import javax.persistence.*
 
 data class OrderResponse(
     val orderId: String,
