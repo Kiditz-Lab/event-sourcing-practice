209285170611f9aa1e7692600ff26f7a9c657344
rifkyaditya <kiditzbastara@gmail.com>
2022-10-19 18:05:27 +0700

    add process payment command

--- order/src/main/kotlin/com/stafsus/practice/order/saga/OrderSaga.kt
+++ order/src/main/kotlin/com/stafsus/practice/order/saga/OrderSaga.kt
@@ -1,10 +1,12 @@
 package com.stafsus.practice.order.saga
 
+import com.stafsus.practice.core.commands.ProcessPaymentCommand
 import com.stafsus.practice.core.commands.ReserveProductCommand
 import com.stafsus.practice.core.events.ProductReservedEvent
 import com.stafsus.practice.core.model.User
 import com.stafsus.practice.core.query.FetchUserPaymentDetailsQuery
 import com.stafsus.practice.order.core.event.OrderCreatedEvent
+import org.apache.commons.lang.StringUtils
 import org.axonframework.commandhandling.callbacks.LoggingCallback
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.messaging.responsetypes.ResponseTypes
@@ -14,6 +16,8 @@ import org.axonframework.queryhandling.QueryGateway
 import org.axonframework.spring.stereotype.Saga
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 @Saga
 class OrderSaga {
@@ -34,10 +38,7 @@ class OrderSaga {
     @SagaEventHandler(associationProperty = "orderId")
     fun handle(event: OrderCreatedEvent) {
         val command = ReserveProductCommand(
-            productId = event.productId,
-            orderId = event.orderId,
-            userId = event.userId,
-            quantity = event.quantity
+            productId = event.productId, orderId = event.orderId, userId = event.userId, quantity = event.quantity
         )
         log.info("OrderCreatedEvent is called for orderId: ${command.orderId}, productId: ${command.productId}")
         commandGateway.send(command, LoggingCallback.INSTANCE)
@@ -47,17 +48,24 @@ class OrderSaga {
     fun handle(event: ProductReservedEvent) {
         log.info("ProductReservedEvent is called for orderId: ${event.orderId}, productId: ${event.productId}")
         val query = FetchUserPaymentDetailsQuery(event.userId)
-        val userPaymentDetails: User? = try {
+        val userPaymentDetails: User = try {
             queryGateway.query(query, ResponseTypes.instanceOf(User::class.java)).join()
         } catch (ex: Exception) {
             log.error(ex.message)
             null
-        }
+        } ?: return
 
-        if (userPaymentDetails == null) {
-            return
-        }
         log.info("Successfully fetch payment details for user : ${userPaymentDetails.firstName}")
+        val paymentCommand = ProcessPaymentCommand(
+            orderId = event.orderId,
+            paymentDetails = userPaymentDetails.paymentDetails,
+            paymentId = UUID.randomUUID().toString(),
+        )
+        val result: String = try {
+            commandGateway.sendAndWait(paymentCommand, 10, TimeUnit.SECONDS)
+        } catch (e: Exception) {
+            StringUtils.EMPTY
+        }
     }
 
 
